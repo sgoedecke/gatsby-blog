ceb1008f9f85c0224351c52f346909e0b4556c42
Sean Goedecke <sgoedecke@zendesk.com>
2020-07-30 15:09:21 +1000

    new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,13 +73,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Sean Goedecke`,
+        short_name: `sean-goedecke`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
