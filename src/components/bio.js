416744e4258151c32356deba3cc1c7418f8e7ca1
Sean Goedecke <sgoedecke@github.com>
2024-12-29 22:20:32 +1100

    bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -40,23 +40,10 @@ const Bio = () => {
         marginBottom: 0,
       }}
     >
-      {/* <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author.name}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      /> */}
       <p>
-        <Link to="/my-engineering-values">recruiters</Link> | {" "}
-        <Link to="/">posts</Link> | <a href="https://docs.google.com/document/d/17Ql6AydCJ7-XhjrwEqzmRAaAaooARTS5vjEhBZGfrbE/edit?usp=sharing">resume</a> |{" "}
-        <a href="https://github.com/sgoedecke">github</a> | <a href="https://www.linkedin.com/in/sean-goedecke-5495a7137/">linkedin</a>
+        <Link to="/my-engineering-values">recruiters</Link> │ {" "}
+        <Link to="/">posts</Link> │ <a href="https://docs.google.com/document/d/17Ql6AydCJ7-XhjrwEqzmRAaAaooARTS5vjEhBZGfrbE/edit?usp=sharing">resume</a> │{" "}
+        <a href="https://github.com/sgoedecke">github</a> │ <a href="https://www.linkedin.com/in/sean-goedecke-5495a7137/">linkedin</a> │ <Link to="/rss.xml">rss</Link>
       </p>
     </div>
   )
