8afabdbd84228f410711af89b32c114d0bba48ff
Sean Goedecke <seangoedecke@gmail.com>
2025-08-10 10:12:25 +1000

    p

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,15 +12,13 @@ const ContactPage = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="Contact" />
 
-      <h2 style={{ marginBottom: rhythm(1) }}>Contact</h2>
-
       <p>
         If you'd like to get in touch, email me at{' '}
         <a href="mailto:sean.goedecke@gmail.com">sean.goedecke@gmail.com</a>. Hearing from readers is one of the main reasons I write at all, so please do reach out if you're thinking about it.
       </p>
 
       <p>
-        You can also message me on <a href="https://www.linkedin.com/in/sean-goedecke-5495a7137/">LinkedIn</a>, though I tend to give quicker responses there.
+        You can also message me on <a href="https://www.linkedin.com/in/sean-goedecke-5495a7137/">LinkedIn</a>, though I tend to give more longer responses over email.
       </p>
 
       <p style={{ marginTop: rhythm(1.5) }}>
